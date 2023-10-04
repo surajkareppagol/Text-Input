77eeb0350a7e9fabf3c2943a18d1c4cd85c04889
surajkareppagol <surajkareppagol.dev@gmail.com>
2023-10-04 18:43:58 +0530

    🌟 Add BackSpace functionality and code type, refactor code

--- index.js
+++ index.js
@@ -6,21 +6,22 @@ const optionList = document.querySelectorAll(".option");
 const optionBold = document.querySelector(".option__b");
 const optionItalics = document.querySelector(".option__i");
 const optionStrikeThrough = document.querySelector(".option__s");
+const optionCode = document.querySelector(".option__c");
 const optionEmoji = document.querySelector(".option__e");
 
 const emojiBox = document.querySelector(".emoji-box");
 const emojiOptions = document.querySelectorAll(".emoji-option");
 
 let formInputValue = [];
-let formInputValueBold = "";
-let formInputValueItalics = "";
-let formInputValueStrikeThrough = "";
-let formInputValueEmoji = "";
-let formInputValueNone = "";
+let formInputTextValue = "";
 
 let currentStyle = "none";
 
 let currentInputStackLength = 0;
+
+if (formInputValue.length === 0) {
+  formDiv.innerHTML = "<p class='form__p-empty'>Start Writing Here...</p>";
+}
 
 function addStyle(option) {
   optionList.forEach((option) => option.classList.remove("select-option"));
@@ -30,91 +31,46 @@ function addStyle(option) {
   option.classList.add("select-option");
 }
 
-optionStrikeThrough.addEventListener("click", (event) => {
-  if (currentStyle === "strikeThrough") {
-    currentStyle = "none";
-    currentInputStackLength = formInputValue.length;
-    formInputValueBold = "";
-    formInputValueItalics = "";
-    formInputValueNone = "";
-
-    addStyle(null);
-    return;
-  }
-
-  currentStyle = "strikeThrough";
-  currentInputStackLength = formInputValue.length;
-  formInputValueBold = "";
-  formInputValueItalics = "";
-  formInputValueNone = "";
-
-  addStyle(optionStrikeThrough);
+optionBold.addEventListener("click", () => {
+  setCurrentStyle("bold", optionBold);
 });
 
-optionBold.addEventListener("click", (event) => {
-  if (currentStyle === "bold") {
-    currentStyle = "none";
-    currentInputStackLength = formInputValue.length;
-    formInputValueStrikeThrough = "";
-    formInputValueItalics = "";
-    formInputValueNone = "";
-
-    addStyle(null);
-    return;
-  }
-
-  currentStyle = "bold";
-  currentInputStackLength = formInputValue.length;
-  formInputValueStrikeThrough = "";
-  formInputValueItalics = "";
-  formInputValueNone = "";
-
-  addStyle(optionBold);
+optionItalics.addEventListener("click", () => {
+  setCurrentStyle("italics", optionItalics);
 });
 
-optionItalics.addEventListener("click", (event) => {
-  if (currentStyle === "italics") {
-    currentStyle = "none";
-    currentInputStackLength = formInputValue.length;
-    formInputValueStrikeThrough = "";
-    formInputValueBold = "";
-    formInputValueNone = "";
-
-    addStyle(null);
-    return;
-  }
-
-  currentStyle = "italics";
-  currentInputStackLength = formInputValue.length;
-  formInputValueStrikeThrough = "";
-  formInputValueBold = "";
-  formInputValueNone = "";
-
-  addStyle(optionItalics);
+optionStrikeThrough.addEventListener("click", () => {
+  setCurrentStyle("strikeThrough", optionStrikeThrough);
 });
 
-optionEmoji.addEventListener("click", (event) => {
-  currentStyle = "emoji";
+optionCode.addEventListener("click", () => {
+  setCurrentStyle("code", optionCode);
+});
 
-  formInputValueStrikeThrough = "";
-  formInputValueBold = "";
-  formInputValueItalics = "";
-
+optionEmoji.addEventListener("click", () => {
   emojiBox.classList.toggle("u-display-flex");
 });
 
 emojiOptions.forEach((emoji) =>
   emoji.addEventListener("click", () => {
-    formInputValueEmoji = emoji;
-    formInputValue.push(formInputValueEmoji.textContent);
+    formInputTextValue = emoji;
+
+    formInputValue.push(formInputTextValue.textContent);
     currentInputStackLength = formInputValue.length;
+
     formDiv.innerHTML = `<p class="form__p">${formInputValue.join(" ")}</p>`;
     emojiBox.classList.remove("u-display-flex");
+
+    formInputTextValue = "";
     currentStyle = "none";
   })
 );
 
 formInput.addEventListener("keydown", (event) => {
+  if (event.key === "Backspace" && formInputValue.length !== 0) {
+    handleBackSpace();
+  }
+
   if (event.key.length === 1) {
     switch (currentStyle) {
       case "none":
@@ -128,28 +84,81 @@ formInput.addEventListener("keydown", (event) => {
         break;
       case "strikeThrough":
         handleFormText(event.key, "strikeThrough");
+        break;
+      case "code":
+        handleFormText(event.key, "code");
     }
   }
 
   formDiv.innerHTML = `<p class="form__p">${formInputValue.join(" ")}</p>`;
+
+  if (formInputValue.length === 0) {
+    formDiv.innerHTML = "<p class='form__p-empty'>Start Writing Here...</p>";
+  }
 });
 
 function handleFormText(key, type) {
   if (formInputValue.length > currentInputStackLength) formInputValue.pop();
 
   if (type === "none") {
-    formInputValueNone += key;
-    formInputValue.push(formInputValueNone);
+    formInputTextValue += key;
+    formInputValue.push(`<span>${formInputTextValue}</span>`);
   } else if (type === "bold") {
-    formInputValueBold += key;
-    formInputValue.push(`<strong>${formInputValueBold}</strong>`);
+    formInputTextValue += key;
+    formInputValue.push(`<strong>${formInputTextValue}</strong>`);
   } else if (type === "italics") {
-    formInputValueItalics += key;
-    formInputValue.push(`<em>${formInputValueItalics}</em>`);
+    formInputTextValue += key;
+    formInputValue.push(`<em>${formInputTextValue}</em>`);
   } else if (type === "strikeThrough") {
-    formInputValueStrikeThrough += key;
+    formInputTextValue += key;
     formInputValue.push(
-      `<span class="u-strike-through">${formInputValueStrikeThrough}</span>`
+      `<span class="u-strike-through">${formInputTextValue}</span>`
     );
+  } else if (type === "code") {
+    formInputTextValue += key;
+    formInputValue.push(`<span class="code">${formInputTextValue}</span>`);
   }
+}
+
+function handleBackSpace() {
+  const lastItem = formInputValue.at(-1);
+
+  const indexOfCloseBracket = lastItem.indexOf(">");
+  const indexOfOpenBracket = lastItem.lastIndexOf("<");
+
+  const tagValue = lastItem
+    .slice(indexOfCloseBracket + 1, indexOfOpenBracket)
+    .slice(0, -1);
+
+  const value = `${lastItem.slice(
+    0,
+    indexOfCloseBracket + 1
+  )}${tagValue}${lastItem.slice(indexOfOpenBracket, lastItem.length + 1)}`;
+
+  formInputValue.pop();
+
+  formInputTextValue = formInputTextValue.slice(0, -1);
+
+  if (!tagValue) {
+    return;
+  }
+
+  formInputValue.push(value);
+}
+
+function setCurrentStyle(style, stylize) {
+  if (currentStyle === style) {
+    currentStyle = "none";
+    currentInputStackLength = formInputValue.length;
+    formInputTextValue = "";
+
+    addStyle(null);
+    return;
+  }
+
+  currentStyle = style;
+  currentInputStackLength = formInputValue.length;
+  formInputTextValue = "";
+
+  addStyle(stylize);
 }
